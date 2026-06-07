1078439ddc81a66a04ce967b0b6e58df5284d8e2
chubenkoam24ithub <chubenkoam24@st.ithub.ru>
2026-06-07 06:36:34 +0300

    Update index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var server_1 = __importDefault(require("./server"));
-var PORT = 1452;
+const PORT = process.env.PORT || 1452;
 server_1.default.listen(PORT, function () {
     console.log("hello on http://localhost:".concat(PORT));
 });
